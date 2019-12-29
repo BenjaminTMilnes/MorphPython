66ddd68405f329fc847cc7edd476380e53a4b057
Benjamin T. Milnes <benjamintmilnes@googlemail.com>
2019-12-29 23:28:17 +0000

    Added more to importer

--- morphe/core.py
+++ morphe/core.py
@@ -61,7 +61,154 @@ def cut(text, startIndex, length=1):
 
 class MImporter (object):
 
-    def _getProperty(inputText, marker):
+    def importDocument(self, inputText):
+        marker = MMarker()
+
+        styleRules = []
+        wasNone = False
+
+        while wasNone == False:
+            sr = self._getStyleRule(inputText, marker)
+
+            if sr == None:
+                break
+            else:
+                styleRules.append(sr)
+
+        d = MDocument()
+
+        d.styleRules = styleRules
+
+        return d
+
+    def _getStyleRule(self, inputText, marker):
+        m = marker.copy()
+
+        selectors = self._getSelectors(inputText, m)
+        properties = self._getProperties(inputText, m)
+
+        if selectors == None or properties == None:
+            return None
+
+        sr = MStyleRule()
+
+        sr.selectors = selectors
+        sr.properties = properties
+
+        marker.p = m.p
+
+        return sr
+
+    def _getSelectors(self, inputText, marker):
+        m = marker.copy()
+
+        self._getWhiteSpace(inputText, m)
+
+        selectors = []
+        wasNone = False
+
+        while wasNone == False:
+
+            s = self._getClassSelector(inputText, m)
+
+            if s != None:
+                selectors.append(s)
+                continue
+
+            s = self._getElementNameSelector(inputText, m)
+
+            if s != None:
+                selectors.append(s)
+                continue
+
+            wasNone = True
+
+        return selectors
+
+    def _getClassSelector(self, inputText, marker):
+        m = marker.copy()
+        t = ""
+
+        c = cut(inputText, m.p)
+
+        if c != ".":
+            return None
+
+        m.p += 1
+
+        while m.p < len(inputText):
+            c = cut(inputText, m.p)
+
+            if c in "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_":
+                t += c
+                m.p += 1
+            else:
+                break
+
+        if len(t) == 0:
+            return None
+
+        s = MClassSelector(t)
+
+        return s
+
+    def _getElementNameSelector(self, inputText, marker):
+        m = marker.copy()
+        t = ""
+
+        while m.p < len(inputText):
+            c = cut(inputText, m.p)
+
+            if c in "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_":
+                t += c
+                m.p += 1
+            else:
+                break
+
+        if len(t) == 0:
+            return None
+
+        s = MElementNameSelector(t)
+
+        return s
+
+    def _getProperties(self, inputText, marker):
+        m = marker.copy()
+
+        self._getWhiteSpace(inputText, m)
+
+        c = cut(inputText, m.p)
+
+        if c != "\{":
+            return None
+
+        m.p += 1
+
+        properties = []
+        wasNone = False
+
+        while wasNone == False:
+            p = self._getProperty(inputText, m)
+
+            if p == None:
+                wasNone = True
+            else:
+                properties.append(p)
+
+        self._getWhiteSpace(inputText, m)
+
+        c = cut(inputText, m.p)
+
+        if c != "\}":
+            return None
+
+        m.p += 1
+
+        marker.p = m.p
+
+        return properties
+
+    def _getProperty(self, inputText, marker):
         m = marker.copy()
 
         self._getWhiteSpace(inputText, m)
@@ -77,6 +224,8 @@ class MImporter (object):
         if c != ":":
             return None
 
+        m.p += 1
+
         value = self._getPropertyValue(inputText, m)
 
         if value == None:
@@ -87,29 +236,15 @@ class MImporter (object):
         if c != ";":
             return None
 
+        m.p += 1
+
+        marker.p = m.p
+
         p = MProperty(name, value)
 
         return p
 
-    def _getPropertyValue(inputText, marker):
-        m = marker
-        t = ""
-
-        while m.p < len(inputText):
-            c = cut(inputText, m.p)
-
-            if c not in ";\{\}":
-                t += c
-                m.p += 1
-            else:
-                break
-
-        if len(t) == 0:
-            return None
-
-        return t
-
-    def _getPropertyName(inputText, marker):
+    def _getPropertyName(self, inputText, marker):
         m = marker
         t = ""
 
@@ -127,7 +262,25 @@ class MImporter (object):
 
         return t
 
-    def _getWhiteSpace(inputText, marker):
+    def _getPropertyValue(self, inputText, marker):
+        m = marker
+        t = ""
+
+        while m.p < len(inputText):
+            c = cut(inputText, m.p)
+
+            if c not in ";\{\}":
+                t += c
+                m.p += 1
+            else:
+                break
+
+        if len(t) == 0:
+            return None
+
+        return t
+
+    def _getWhiteSpace(self, inputText, marker):
         m = marker
         t = ""
 
