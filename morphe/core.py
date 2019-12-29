62a9782bfce93fc61d799fdf30e37543bace859a
Benjamin T. Milnes <benjamintmilnes@googlemail.com>
2019-12-29 17:43:48 +0000

    Began adding importer

--- morphe/core.py
+++ morphe/core.py
@@ -30,3 +30,64 @@ class MDocument(object):
     def __init__(self):
 
         self.styleRules = []
+
+
+class MMarker (object):
+    def __init__(self):
+
+        self.position = 0
+
+    @property
+    def p(self):
+        return self.position
+
+    @p.setter
+    def p(self, value):
+        self.position = value
+
+def cut(text, startIndex, length = 1):
+    a = startIndex
+    b = startIndex + length
+    return text[a:b]
+
+class MImporter (object):
+
+    def _getPropertyName(inputText, marker):
+        m = marker
+        t = ""
+
+        while m.p < len(inputText):
+            c = cut(inputText, m.p)
+
+            if c in "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-":
+                t += c
+                m.p += 1
+            else:
+                break
+
+        if len(t) == 0:
+            return None
+
+        return t
+
+
+
+
+
+    def _getWhiteSpace(inputText, marker):
+        m = marker
+        t = ""
+
+        while m.p < len(inputText):
+            c = cut(inputText, m.p)
+
+            if c in " \t\n":
+                t += c
+                m.p += 1
+            else:
+                break
+
+        if len(t) == 0:
+            return None
+
+        return t
