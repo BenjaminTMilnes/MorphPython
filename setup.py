527418db9fba7b482a8eeaba468ed8a0df6e805f
Benjamin T. Milnes <benjamintmilnes@googlemail.com>
2020-01-06 23:23:13 +0000

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+with open("README.md", "r") as fo:
+    long_description = fo.read()
+
+setuptools.setup(
+    name="morphe",
+    version="0.2.0",
+    author="B. T. Milnes"
+    description="A Morphe implementation in Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/BenjaminTMilnes/MorphePython",
+    packages=["morphe"]
+)
