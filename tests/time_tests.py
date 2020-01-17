1f45f16c6208b33d98d2c1bedfd8f06773d46444
Benjamin T. Milnes <benjamintmilnes@googlemail.com>
2020-01-17 00:19:42 +0000

    Added simple time tests

---
+++ tests/time_tests.py
@@ -0,0 +1,30 @@
+
+from morphe.core import *
+
+import timeit
+
+n = 1000
+
+t = timeit.timeit(lambda: importMorpheDocumentFromFile("examples/example1.morphe"), number=n)
+
+print(t/n)
+
+example1 = """
+
+p {
+    font-size: 12pt;
+    font-colour   :   black   ;
+    margin   :12pt 16pt   ;
+}
+
+.red { font-colour: red; }
+
+#infobox {
+    border: 1px solid blue;
+}
+
+"""
+
+t = timeit.timeit(lambda: importMorpheDocument(example1), number=n)
+
+print(t/n)
